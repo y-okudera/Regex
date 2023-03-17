f7af542f6de1c7133af80095ab08a927030709bc
yuoku <appledev.yuoku@gmail.com>
2023-03-18 02:00:30 +0900

    Fix EmailValidateUseCase.

--- Regex/UseCase/EmailValidateUseCase.swift
+++ Regex/UseCase/EmailValidateUseCase.swift
@@ -23,20 +23,9 @@ private struct EmailValidateInteractor: EmailValidateUseCase {
     func execute(email: String) -> Bool {
 
         // 正規表現パターン
-        let pattern = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}"
+        let pattern = #"[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}"#
 
-        // 正規表現オブジェクトの生成
-        guard let regex = try? NSRegularExpression(pattern: pattern, options: .caseInsensitive) else {
-            return false
-        }
-
-        // 入力文字列に一致する範囲を取得
-        let range = NSRange(location: 0, length: email.utf16.count)
-
-        // 正規表現に一致する文字列を検索
-        let matches = regex.matches(in: email, options: [], range: range)
-
-        return matches.count > 0
+        return email.range(of: pattern, options: .regularExpression) != nil
     }
 
 }
